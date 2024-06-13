5a3f3d67c10e80af89c61cab1e9ed9ec14a62dde
davidortuno <ldtorresortuno@gmail.com>
2024-06-13 17:01:14 -0600

    Se integra función de SimpleLightbox

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+
+  var popupBox = new SimpleLightbox('a.popupBox', {
+    // var $gallery = new SimpleLightbox('.gallery a', {
+    loop: false,
+    doubleTapZoom: false,
+    scrollZoom: false,
+    showCounter: false,
+  });
+
+
